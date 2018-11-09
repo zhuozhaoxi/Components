fb5633141aee217bb3b648a3231ab56e1f2ba77c
zhaoxi <zhuozhaoxi@jd.com>
2018-11-09 17:12:52 +0800

    增加动态滚动组件

---
+++ DynamicScroll/demo.vue
@@ -0,0 +1,37 @@
+<template>
+  <dynamic-scroll :list="cells" :scrollViewHeight="736">
+    <div slot="cell" slot-scope="props"
+         :style="props.cell.style">{{props.cell.text}}</div>
+  </dynamic-scroll>
+</template>
+
+<script>
+  import DynamicScroll from './index'
+
+  export default {
+    name: 'Scroll',
+    components: { DynamicScroll },
+    computed: {
+      cells () {
+        return new Array(1000).fill(1).map((item, index) => {
+          return {
+            style: {
+              height: Math.floor(Math.random() * 100 + 100) + 'px',
+              // height: '300px',
+              color: '#ffffff',
+              fontSize: '30px',
+              background: this.getRandomColor()
+            },
+            text: '#' + (index + 1)
+          }
+        })
+      }
+    },
+    methods: {
+      getRandomColor () {
+        const colors = new Array(3).fill(1).map(item => Math.floor(Math.random() * 255))
+        return `rgb(${colors.join(',')})`
+      }
+    }
+  }
+</script>
---
+++ DynamicScroll/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="t-scroll"
+       ref="$scroll"
+       :style="{ height: this.scrollViewHeight + 'px' }"
+       @scroll.passive="onScroll">
+    <div class="t-scroll-padding-top" :style="{height: scrollData.paddingTop + 'px'}"></div>
+
+    <div ref="$cell" v-for="item in scrollData.displayCells">
+      <slot name="cell" :cell="item"></slot>
+    </div>
+
+    <div class="t-scroll-padding-bottom" :style="{height: scrollData.paddingBottom + 'px'}"></div>
+  </div>
+</template>
+
+<script>
+  import ScrollManager from './ScrollManager'
+
+  let manager
+  let lastScrollTop
+  let heightFixed = true
+  let t
+  let lastRunTime
+
+  export default {
+    name: 'DynamicScroll',
+    props: {
+      scrollViewHeight: {
+        type: Number,
+        required: true
+      },
+
+      list: {
+        type: Array,
+        required: true
+      },
+      // cell缓存数量 即不在可视区域内的预加载数量
+      cellCacheNumber: {
+        type: Number,
+        default: 3
+      },
+      // cell高度值 如果为0或不传则为动态高度 不为0则为固定高度
+      cellHeight: {
+        type: Number,
+        default: 0
+      },
+
+    },
+    data () {
+      return {
+        scrollData: {
+          scrollHeight: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+
+          displayCells: []
+        },
+      }
+    },
+
+    methods: {
+      initScrollManager () {
+        manager = new ScrollManager({
+          list: this.list,
+          scrollViewHeight: this.scrollViewHeight,
+          cellHeight: this.cellHeight,
+          cellCacheNumber: this.cellCacheNumber,
+          firstRenderNumber: 10
+        })
+      },
+
+      updateScrollRender (init = false) {
+        this.scrollData = manager.getRenderInfo()
+        this.$forceUpdate()
+        // 更新完成后矫正滚动条位置
+        this.$nextTick(() => {
+          this.$refs.$scroll.scrollTop = lastScrollTop;
+          if (!heightFixed) {
+            manager.updateCellHeight(
+              this.$refs.$cell.map(item => item.getBoundingClientRect().height)
+            )
+            // 动态高度时， 初次初始化时计算滑动的总高度
+            if(init){
+              manager.updateScrollHeight()
+              this.scrollData = manager.getRenderInfo()
+            }
+          }
+        })
+      },
+
+
+      onScroll () {
+        clearTimeout(t)
+        t = setTimeout(()=>{
+          this.handleScroll(true)
+        }, 300)
+        let current = new Date().getTime()
+        if(lastRunTime === undefined || current - lastRunTime > 100){
+          lastRunTime = current
+          this.handleScroll()
+        }
+
+      },
+
+      handleScroll(updateScrollHeight = false){
+        lastScrollTop = this.$refs.$scroll.scrollTop;
+        manager.updateScroll(lastScrollTop);
+        this.updateScrollRender(updateScrollHeight)
+      },
+    },
+    watch: {
+      list () {
+        lastScrollTop = 0
+        this.initScrollManager()
+        this.updateScrollRender(true)
+      }
+    },
+    mounted () {
+      if (!this.cellHeight) heightFixed = false
+      this.initScrollManager()
+      this.updateScrollRender(true)
+    }
+  }
+</script>
+
+<style scoped>
+  .t-scroll  {
+    position: relative;
+    background: #eeeeee;
+    overflow: scroll;
+  }
+  .t-scroll-cell {
+    color: #ffffff;
+    font-size: 30px;
+    font-weight: bolder;
+  }
+</style>
