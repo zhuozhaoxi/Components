a5cfe5f7eb284c6723d36c47e9f33a3436a094e0
zhaoxi <zhuozhaoxi@jd.com>
2018-11-14 17:22:45 +0800

    增加demo

---
+++ JsonView/demo.vue
@@ -0,0 +1,147 @@
+<template>
+	<json-view :data="json"></json-view>
+</template>
+
+<script>
+  import JsonView from './index'
+  import DynamicScroll from 'Components/DynamicScroll'
+
+  export default {
+    name: 'Scroll',
+    components: { DynamicScroll,JsonView },
+    data(){
+      return {
+        json: {
+          a: 1,
+          b: true,
+          c: false,
+          d: undefined,
+          e: null,
+          f: '<span>for html test</span>',
+          g: '"string"',
+          h: [
+            1,
+            true,
+            false,
+            undefined,
+            null,
+            'string',
+            {
+              a: 1,
+              b: true,
+              c: false,
+              d: undefined,
+              e: null,
+              f: 'string',
+              g: '"string"',
+            },
+            {
+              a: 1,
+              b: true,
+              c: false,
+              d: undefined,
+              e: null,
+              f: 'string',
+              g: '"string"',
+              h: [
+                1,
+                true,
+                false,
+                undefined,
+                null,
+                'string',
+                {
+                  a: 1,
+                  b: true,
+                  c: false,
+                  d: undefined,
+                  e: null,
+                  f: 'string',
+                  g: '"string"',
+                }
+              ]
+            },
+            {
+              a: 1,
+              b: true,
+              c: false,
+              d: undefined,
+              e: null,
+              f: 'string',
+              g: '"string"',
+              h: [
+                1,
+                true,
+                false,
+                undefined,
+                null,
+                'string',
+                {
+                  a: 1,
+                  b: true,
+                  c: false,
+                  d: undefined,
+                  e: null,
+                  f: 'string',
+                  g: '"string"',
+                }
+              ]
+            },
+            {
+              a: 1,
+              b: true,
+              c: false,
+              d: undefined,
+              e: null,
+              f: 'string',
+              g: '"string"',
+              h: [
+                1,
+                true,
+                false,
+                undefined,
+                null,
+                'string',
+                {
+                  a: 1,
+                  b: true,
+                  c: false,
+                  d: undefined,
+                  e: null,
+                  f: 'string',
+                  g: '"string"',
+                }
+              ]
+            },
+            {
+              a: 1,
+              b: true,
+              c: false,
+              d: undefined,
+              e: null,
+              f: 'string',
+              g: '"string"',
+              h: [
+                1,
+                true,
+                false,
+                undefined,
+                null,
+                'string',
+                {
+                  a: 1,
+                  b: true,
+                  c: false,
+                  d: undefined,
+                  e: null,
+                  f: 'string',
+                  g: '"string"',
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  }
+</script>
